d1c783df7bfe668f81d69744f325423c840e9ac4
JaydipJadhav25 <jaydipdjadhav@gmail.com>
2025-11-21 14:43:14 +0530

    add animation

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,13 +1,16 @@
-import React from 'react'
+import React, { useEffect } from 'react' // Import useEffect
 import '../styles/home.css'
 import { Container, Row, Col } from 'reactstrap'
 import heroImg from '../assets/images/hero-img01.jpg'
 import heroImg02 from '../assets/images/hero-img02.jpg'
 import heroVideo from '../assets/images/hero-video.mp4'
-import WorldImg from '../assets/images/world.png'
 import experinceImg from '../assets/images/experience.png'
 
-import Subtitle from '../shared/Subtitle'  
+
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
+import Subtitle from '../shared/Subtitle'
 import SearchBar from '../shared/SearchBar'
 import ServicesList from '../services/ServicesList'
 import FeaturedToursList from '../components/featured-tours/FeaturedToursList'
@@ -18,17 +21,28 @@ import NewsLetter from '../shared/NewsLetter'
 
 
 const Home = () => {
+ 
+  useEffect(() => {
+    Aos.init({
+      duration: 1000, // Duration of animation in ms
+      once: true, // Whether animation should happen only once - while scrolling down
+    });
+    Aos.refresh(); 
+  }, []);
+
+
   return (
     <>
       {/*============== Hero Start==========*/}
       <section>
         <Container>
           <Row>
+            {/* The main hero content will fade up */}
             <Col lg='6'>
-              <div className="hero__content">
+              <div className="hero__content" data-aos="fade-right">
                 <div className="hero__subtitle d-flex align-items-center">
                   <Subtitle Subtitle={'Know Before you GO'} />
-                  <img src={WorldImg} alt="" />
+                  {/* <img src="/" alt="" /> */}
                 </div>
 
                 <h1>
@@ -38,26 +52,27 @@ const Home = () => {
 
                 <p>
                   Lorem ipsum dolor sit amet consectetur adipisicing elit.
-                  Numquam sed harum recusandae corrupti at esse ex praesentium saepe 
+                  Numquam sed harum recusandae corrupti at esse ex praesentium saepe
                   illum dolorem nam dicta aliquam, maiores accusamus voluptate atque vitae?
                 </p>
               </div>
             </Col>
 
+            {/* The images/video boxes will stagger for effect */}
             <Col lg='2'>
-              <div className="hero__img-box">
+              <div className="hero__img-box" data-aos="fade-down" data-aos-delay="200">
                 <img src={heroImg} alt="" />
               </div>
             </Col>
 
             <Col lg='2'>
-              <div className="hero__img-box mt-4">
+              <div className="hero__img-box mt-4" data-aos="fade-down" data-aos-delay="400">
                 <video src={heroVideo} controls />
               </div>
             </Col>
 
             <Col lg='2'>
-              <div className="hero__img-box mt-5">
+              <div className="hero__img-box mt-5" data-aos="fade-down" data-aos-delay="600">
                 <img src={heroImg02} alt="" />
               </div>
             </Col>
@@ -66,84 +81,88 @@ const Home = () => {
         </Container>
       </section>
       {/*============== Hero End==========*/}
+
+      {/*============== Services Section Start==========*/}
       <section>
       <Container>
         <Row>
-          <Col lg='3'>
-    <h5 className="services__subtitle">What we search</h5>
-    <h2 className='services__title'>We offer our best services</h2>
+          <Col lg='3' data-aos="fade-right">
+            <h5 className="services__subtitle">What we search</h5>
+            <h2 className='services__title'>We offer our best services</h2>
           </Col>
+          {/* ServicesList will contain elements that can have their own animations applied */}
           <ServicesList/>
         </Row>
-        
       </Container>
       </section>
+      {/*============== Services Section End==========*/}
 
-      {/* =============fertured tour section start */}
+      {/* =============Featured Tour Section Start============= */}
       <Container>
         <Row>
-          <Col lg='12' className='mb-5'>
-          <Subtitle subtitle={'Explore'}/>
-          <h2 className="featured__tour-title">Our Featured Tours</h2> 
+          <Col lg='12' className='mb-5' data-aos="zoom-in">
+            <Subtitle subtitle={'Explore'}/>
+            <h2 className="featured__tour-title">Our Featured Tours</h2>
           </Col>
+          {/* FeaturedToursList will contain animated tour cards */}
           <FeaturedToursList/>
         </Row>
       </Container>
-       {/* =============fertured tour section end */}
+       {/* =============Featured Tour Section End============= */}
 
-        {/* =============Experience section start */}
+        {/* =============Experience Section Start============= */}
         <section>
           <Container>
             <Row>
               <Col lg='6'>
-              <div className="experience__content">
-                <Subtitle subtitle={'Experience'}/>
-                <h2>With Our All Experince <br />We Will Serve</h2>
-                <p>
-                  Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti similique nisi dolorum perferendis,
-                  <br/> error, unde blanditiis at quae voluptates rem quisquam? Optio modi perspiciatis libero earum eligendi quod delectus tempore!
-                </p>
-              </div>
-              <div className="counter__wrapper d-flex align-item-center gap-5">
-                <div className="counter__box">
-                  <span>12k+</span>
-                  <h6>Successfull Trip</h6>
+                <div className="experience__content" data-aos="fade-right">
+                  <Subtitle subtitle={'Experience'}/>
+                  <h2>With Our All Experince <br />We Will Serve</h2>
+                  <p>
+                    Lorem ipsum dolor sit, amet consectetur adipisicing elit. Corrupti similique nisi dolorum perferendis,
+                    <br/> error, unde blanditiis at quae voluptates rem quisquam? Optio modi perspiciatis libero earum eligendi quod delectus tempore!
+                  </p>
                 </div>
-
+                <div className="counter__wrapper d-flex align-item-center gap-5" data-aos="fade-up" data-aos-delay="300">
                   <div className="counter__box">
-                  <span>2k+</span>
-                  <h6>Regular Trip</h6>
-                </div>
+                    <span>12k+</span>
+                    <h6>Successfull Trip</h6>
+                  </div>
 
-                  <div className="counter__box">
-                  <span>12</span>
-                  <h6>Years Experince</h6>
+                    <div className="counter__box">
+                    <span>2k+</span>
+                    <h6>Regular Trip</h6>
+                  </div>
+
+                    <div className="counter__box">
+                    <span>12</span>
+                    <h6>Years Experince</h6>
+                  </div>
                 </div>
-              </div>
               </Col>
 
                <Col lg='6'>
-                <div className="experience__img">
-                  <img src={experinceImg}></img>
+                <div className="experience__img" data-aos="zoom-in" data-aos-delay="400">
+                  <img src={experinceImg} alt="Experience"></img>
                 </div>
               </Col>
             </Row>
           </Container>
         </section>
-
-         {/* =============Experience section end */}
+         {/* =============Experience Section End============= */}
 
          {/* ============ Gallery Section Start============= */}
           <section>
             <Container>
               <Row>
-                <Col lg='12'>
-                <Subtitle subtitle={'Gallery'}/>
-                <h2 className="gallery__title">Visit Our Customers Trip Gallery</h2>
-                
+                <Col lg='12' data-aos="fade-up">
+                  <Subtitle subtitle={'Gallery'}/>
+                  <h2 className="gallery__title">Visit Our Customers Trip Gallery</h2>
+
                 </Col>
 
                 <Col lg='12'>
+                {/* MasonaryImagesGallery will contain animated images */}
                 <MasonaryImagesGallery/>
                 </Col>
               </Row>
@@ -151,23 +170,24 @@ const Home = () => {
           </section>
          {/* ============ Gallery Section End============= */}
 
-         {/* ======== Testimonail Section Start */}
+         {/* ======== Testimonial Section Start */}
          <section>
           <Container>
             <Row>
-              <Col lg='12'>
-              <Subtitle subtitle={'Fans Love'}/>
-              <h2 className="testomonial__title">What About Our Fans Say Us</h2>
+              <Col lg='12' data-aos="fade-up">
+                <Subtitle subtitle={'Fans Love'}/>
+                <h2 className="testomonial__title">What About Our Fans Say Us</h2>
 
               </Col>
               <Col lg='12'>
-              <Testimonials/>
+                {/* Testimonials component likely contains a carousel/slider, which can have its own animation */}
+                <Testimonials/>
               </Col>
             </Row>
           </Container>
          </section>
-           {/* ======== Testimonail Section End */}
-
+           {/* ======== Testimonial Section End */}
+           <NewsLetter/>
     </>
   )
 }
--- src/components/featured-tours/FeaturedToursList.jsx
+++ src/components/featured-tours/FeaturedToursList.jsx
@@ -1,20 +1,40 @@
-import React from 'react'
+import React, { useEffect } from 'react' // <--- ADDED 'useEffect' HERE
 import TourCard from '../../shared/TourCard'
 import tourData from '../../assets/data/tours'
 import { Col } from 'reactstrap'
+import Aos from 'aos'
+import 'aos/dist/aos.css'
+
 
 const FeaturedToursList = () => {
+
+
+    useEffect(() => {
+      Aos.init({
+        duration: 1000, 
+        once: true, 
+      });
+      Aos.refresh(); 
+    }, []);
+  
+
   return (
     <>
     {
-        tourData?.map(tour=>(
-            <Col lg='3' className='mb-4' key={tour.id}>
-              <TourCard tour={tour}></TourCard>
-
-            </Col>
-        ))
+    
+      tourData?.map((tour, index) => (
+        
+        <Col 
+          lg='3' 
+          className='mb-4' 
+          key={tour.id}
+          data-aos="fade-up" 
+          data-aos-delay={`${index * 100}`} 
+        >
+          <TourCard tour={tour}></TourCard>
+        </Col>
+      ))
     }
-   
     </>
   )
 }
